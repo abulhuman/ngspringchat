100380edabb225a7db522d1dfbd3e7fc9c84702f
Adem Mohammed <45444428+abulhuman@users.noreply.github.com>
2023-07-17 10:41:08 +0300

    Initial Commit

---
+++ src/main/java/com/company/ngspringchat/NgSpringChatApplication.java
@@ -0,0 +1,13 @@
+package com.company.ngspringchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgSpringChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NgSpringChatApplication.class, args);
+	}
+
+}
